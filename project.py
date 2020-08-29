1242af1e84de91928d6e9d8ef0b0bb9416e61767
tosbaa <yagizkocer@gmail.com>
2020-08-29 14:47:51 +0300

    Put all the comparison algorithms and managed to fit the pd data

--- project.py
+++ project.py
@@ -13,11 +13,13 @@ data = pd.read_csv('HappinessAlcoholConsumption.csv')
 data = np.array([data['HDI'], data['Spirit_PerCapita']])
 data = np.stack((data[0], data[1]), axis=-1)
 
-# plt.scatter(data[0], data[1], **plot_kwds)
+
+# plt.scatter(data.T[0], data.T[1], **plot_kwds)
 # frame = plt.gca()
 # frame.axes.get_xaxis().set_visible(False)
 # frame.axes.get_yaxis().set_visible(False)
-# plt.show()
+plt.xlabel("Happiness Score")
+plt.ylabel("Spirit Drink Consumption")
 
 
 def plot_clusters(data, algorithm, args, kwds):
@@ -28,12 +30,38 @@ def plot_clusters(data, algorithm, args, kwds):
     colors = [palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in labels]
     plt.scatter(data.T[0], data.T[1], c=colors, **plot_kwds)
     frame = plt.gca()
-    frame.axes.get_xaxis().set_visible(False)
-    frame.axes.get_yaxis().set_visible(False)
+    # frame.axes.get_xaxis().set_visible(False)
+    # frame.axes.get_yaxis().set_visible(False)
     plt.title('Clusters found by {}'.format(str(algorithm.__name__)), fontsize=24)
-    plt.text(-0.5, 0.7, 'Clustering took {:.2f} s'.format(end_time - start_time), fontsize=14)
+    print('Clustering took {:.2f} s'.format(end_time - start_time))
 
-plot_clusters(data, cluster.KMeans, (), {'n_clusters':6})
+
+## K means
+#plot_clusters(data, cluster.KMeans, (), {'n_clusters':4})
+##
+
+## Affinity Propagation
+#plot_clusters(data, cluster.AffinityPropagation, (), {'preference':-1000, 'damping':0.95})
+##
+
+##Mean Shift
+#plot_clusters(data, cluster.MeanShift, (100,), {'cluster_all':False})
+##
+
+##Agloromerative
+#plot_clusters(data, cluster.AgglomerativeClustering, (), {'n_clusters':6, 'linkage':'ward'})
+##
+
+##DBSCAN
+#plot_clusters(data, cluster.DBSCAN, (), {'eps':30})
+##
+
+##HDBSCAN
+#import hdbscan
+#plot_clusters(data, hdbscan.HDBSCAN, (), {'min_cluster_size':4})
+##
+
 plt.show()
+
 
 
